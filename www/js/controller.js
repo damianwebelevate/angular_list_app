928c7f47a97a554565ab94c08d2a4aaf704429a6
damianwebelevate <damian.orourke@webelevateie>
2014-05-13 11:50:32 +0100

    redid using model instead of controller for full MVC

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,6 +1,8 @@
-function ToDoController($scope){
+var todolist = angular.module('todolist', [])
 
-$scope.ToDo = [{Task: 'Task 1' , Details: 'This is an example of an angular.js application that I will use on a phonegap build.  Just checking out the capabilities of the storage and the outcome by using <p> and </p> tags.  I just discovered it has no effect!!!'},
+todolist.controller('ToDoController', function ($scope){
+
+$scope.items = [{Task: 'Task 1' , Details: 'This is an example of an angular.js application that I will use on a phonegap build.  Just checking out the capabilities of the storage and the outcome by using <p> and </p> tags.  I just discovered it has no effect!!!'},
 			   {Task: 'Task 2' , Details: 'Some Other Details'},
 			   {Task: 'Task 3' , Details: 'More of the same'}
 			   ];
@@ -8,7 +10,7 @@ $scope.ToDo = [{Task: 'Task 1' , Details: 'This is an example of an angular.js a
 
 
 $scope.addATask = function(){
-	$scope.ToDo.push(
+	$scope.items.push(
 	{
 		Task: $scope.addTask.task,
 		Details: $scope.addTask.details,
@@ -21,9 +23,10 @@ $scope.addATask = function(){
 //remove a task
 
  $scope.removeItem = function(index){
-    $scope.ToDo.splice(index, 1);
+    $scope.items.splice(index, 1);
 }
 
 
-}//close scope
+
+});//close scope
 
